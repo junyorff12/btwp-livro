80cea0af9e931a0d6fdbfe933ef8c1f037c581dd
junyorff12 <junyorff12@gmail.com>
2016-12-12 00:50:45 -0200

    update1

---
+++ CAP 3/bootstrappin-theme/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="content-info container" role="contentinfo">
+  <div class="row">
+    <div class="col-lg-12">
+      <p><a href="<?php echo home_url(); ?>/"><img src="<?php
+ echo get_template_directory_uri();
+ ?>/assets/img/logo.png" width="80"
+ alt="Bootstrappin'"></a></p>
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+      
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
